09624c1f2e5de2b15c924ed7b997cd80829d7fab
Javier Herrero <Javier.Herrero@tgac.ac.uk>
2013-11-06 14:13:12 +0000

    spaces to tab

--- count-read-bp.py
+++ count-read-bp.py
@@ -4,17 +4,17 @@ import screed
 import sqer
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='+')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filenames', nargs='+')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    total = 0
-    for filename in args.filenames:
-        records = screed.open(filename)
-        total += sqer.sum_bp_records(records)
+	total = 0
+	for filename in args.filenames:
+		records = screed.open(filename)
+		total += sqer.sum_bp_records(records)
 
-    print '%d bp total' % total
+	print '%d bp total' % total
 
 if __name__ == '__main__':
-    main()
+	main()
